d27f0d465d479bc82da85d5f75e61263f7a822c2
hichamatlas75-del <hichamatlas75@gmail.com>
2026-09-26 12:18:31 +0100

    Add files via upload

--- js/plannings.js
+++ js/plannings.js
@@ -100,7 +100,7 @@ const TeamService = (() => {
       KHAOULA: { hP: "07:00", off: false, shift: "07h — 15h" },
       FATIMA:  { hP: "07:00", off: false, shift: "07h — 15h" },
       JIHANE:  { hP: "14:00", off: false, shift: "14h — F.S" },
-      IMANE:   { hP: "10:00", off: false, shift: "10h — 18h" },
+      IMANE:   { hP: "11:00", off: false, shift: "10h — 18h" },
       ANAS:    { hP: "14:00", off: false, shift: "14h — F.S" },
       JAWAD:   { hP: "14:00", off: false, shift: "14h — F.S" },
       SAAD:    { hP: "13:00", off: false, shift: "13h — F.S" }
@@ -110,7 +110,7 @@ const TeamService = (() => {
       KHAOULA: { hP: "07:00", off: false, shift: "07h — 15h" },
       FATIMA:  { hP: "07:00", off: false, shift: "07h — 15h" },
       JIHANE:  { hP: "14:00", off: false, shift: "14h — F.S" },
-      IMANE:   { hP: "10:00", off: false, shift: "10h — 18h" },
+      IMANE:   { hP: "11:00", off: false, shift: "10h — 18h" },
       ANAS:    { hP: "14:00", off: false, shift: "14h — F.S" },
       JAWAD:   { hP: "14:00", off: false, shift: "14h — F.S" },
       SAAD:    { hP: "14:00", off: false, shift: "14h — 21h" }
